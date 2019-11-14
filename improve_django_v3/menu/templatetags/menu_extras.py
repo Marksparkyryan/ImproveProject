72c66319f2f9f41dc7f75301f083817be31d38db
Mark Ryan <markryandeveloper@gmail.com>
2019-11-13 21:22:09 -0500

    added menu detail template components

--- improve_django_v3/menu/templatetags/menu_extras.py
+++ improve_django_v3/menu/templatetags/menu_extras.py
@@ -7,8 +7,11 @@ from ..models import Menu
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True, name='menu_years')
-def menu_years(context):
+@register.simple_tag(name='menu_years')
+def menu_years():
+    """Retrieve lowest expiration year in all menu objects, then create 
+    and return a range of years from then up to now
+    """
     query = Menu.objects.values(
         'expiration_date'
     ).aggregate(
